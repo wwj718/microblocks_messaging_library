e5d70983dd64c5d87ee7995edb4df9d1ea3c8d44
wwj718 <wuwenjie718@gmail.com>
2022-10-28 07:23:46 +0800

    tweak the output(receiveBroadcasts); Formatting tweaks;

--- microblocks_messaging_library/__init__.py
+++ microblocks_messaging_library/__init__.py
@@ -1,9 +1,21 @@
+# John Maloney, October 2022
+# Revised by Wenjie Wu, October 2022
+
+__version__ = "0.0.1"
+
+
 import serial
 
+
 class MicroblocksMessage:
-    def __init__(self):
+    def __init__(self, verbose=False):
         self.ser = None
         self._buffer = bytearray()
+        self._verbose = verbose  # verbose: Print various debugging information
+        self.on_message = None  # paho style:  https://www.eclipse.org/paho/index.php?page=clients/python/docs/index.php
+
+    def __del__(self):
+        self.disconnect()
 
     def connect(self, port):
         self.ser = serial.Serial(port, 115200)
@@ -12,9 +24,11 @@ class MicroblocksMessage:
         self.ser.close()
 
     def sendBroadcast(self, aString):
-        utf8 = aString.encode('utf-8')
+        utf8 = aString.encode("utf-8")
         length = len(utf8) + 1
-        bytes = bytearray([251, 27, 0, length % 256, int(length / 256)]) + utf8 + b'\xfe'
+        bytes = (
+            bytearray([251, 27, 0, length % 256, int(length / 256)]) + utf8 + b"\xfe"
+        )
         self.ser.write(bytes)
 
     def receiveBroadcasts(self):
@@ -23,8 +37,11 @@ class MicroblocksMessage:
         if data:
             self._buffer = self._buffer + data
             for msgBytes in self._match(27):
-                result.append(msgBytes[4 :].decode("utf-8"))
-        return result
+                result.append(msgBytes[4:].decode("utf-8").replace("\x00", ""))
+        if result == []:
+            return None
+        else:
+            return "".join(result)
 
     def _match(self, filter="*"):
         buf = self._buffer
@@ -37,25 +54,25 @@ class MicroblocksMessage:
         i = 0
         while True:
             while not ((i >= length) or (buf[i] == 250) or (buf[i] == 251)):
-                i += 1 # skip to start of next message
+                i += 1  # skip to start of next message
             bytesRemaining = length - i
-            if (bytesRemaining < 1): # nothing to process
-                self._buffer = buf[i :]
+            if bytesRemaining < 1:  # nothing to process
+                self._buffer = buf[i:]
                 return result
             cmd = buf[i]
-            if (cmd == 250) and (bytesRemaining >= 3): # short message (3 bytes)
-                if (filter == '*') or (filter == buf[i + 1]):
+            if (cmd == 250) and (bytesRemaining >= 3):  # short message (3 bytes)
+                if (filter == "*") or (filter == buf[i + 1]):
                     result.append(buf[i : i + 3])
                 i += 3
-            elif (cmd == 251) and (bytesRemaining >= 5): # long message (>= 5 bytes)
+            elif (cmd == 251) and (bytesRemaining >= 5):  # long message (>= 5 bytes)
                 msgLen = (256 * buf[i + 4]) + buf[i + 3]
                 end = i + 5 + msgLen
-                if end > length: # long message is not yet complete
-                    self._buffer = buf[i :]
+                if end > length:  # long message is not yet complete
+                    self._buffer = buf[i:]
                     return result
-                if (filter == '*') or (filter == buf[i + 1]):
-                    result.append(buf[i : end])
+                if (filter == "*") or (filter == buf[i + 1]):
+                    result.append(buf[i:end])
                 i = end
             else:
-                self._buffer = buf[i :]
+                self._buffer = buf[i:]
                 return result
